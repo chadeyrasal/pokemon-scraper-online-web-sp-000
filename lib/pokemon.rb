8c38c772dfa563d0d5eaa42f4485fdfe570779e1
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 19:38:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,8 +13,8 @@ class Pokemon
     @@all << self
   end
 
-  def self.save(name, type, database_connection)
-    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
   def self.all
